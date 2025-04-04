9fa520355aee2e7dfa38021031bc238873d481db
sandip rai <iamsandip-git>
2025-04-04 14:17:26 +0545

    edit actions

--- api/admin.py
+++ api/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import News
 
 
-admin.site.register(News)
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'source',)  # Optional: to show these fields in list view
+    list_filter = ('source',)  # This enables the filter by 'source' in the sidebar
+admin.site.register(News, NewsAdmin)
